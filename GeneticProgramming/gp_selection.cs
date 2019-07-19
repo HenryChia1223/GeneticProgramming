811ab7b8bdbb909eb8a61dceb10ac1ee89d1b8e6
HenryChia <scenic831223@gmail.com>
2019-07-19 21:08:09 +0800

    second commit

---
+++ GeneticProgramming/gp_selection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticProgramming
+{
+    class gp_selection
+    {
+        public static void selection(Dictionary<string,int> dic)
+        {
+
+        } 
+    }
+}
